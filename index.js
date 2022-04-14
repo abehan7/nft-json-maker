96d2a84a6921e6d1c87e8bc0403e10b63d9b6a26
unknown <hanjk13262@gmail.com>
2022-04-14 20:00:11 +0900

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const nftName = "K-Birdz Egg";
-const description = "K-Birdz, Flappy Bird Style P2E game with K-culture made by youtuber JoCoding";
-const hiddenImgUrl = "ipfs://QmbbfqR9EHdEY2Jy2dJYx8jjFftoaMnD6vgKJLMFXemVdL/hidden.mp4";
-const totalNum = 5;
+const description =
+  "K-Birdz, Flappy Bird Style P2E game with K-culture made by youtuber JoCoding";
+const hiddenImgUrl =
+  "ipfs://QmbbfqR9EHdEY2Jy2dJYx8jjFftoaMnD6vgKJLMFXemVdL/hidden.mp4";
+const totalNum = 100;
 
 try {
-    for (let i = 1; i <= totalNum; i++) {
-        let json = `{"name":"${nftName} #${i}","description":"${description}","image":"${hiddenImgUrl}","attributes":[{"trait_type": "Unknown","value": "Unknown"}]}`;
-        let fs = require("fs");
-        fs.writeFile(`json/${i}.json`, json, "utf8", (e)=>(e));
-    }
-    console.log("complete!");
+  for (let i = 1; i <= totalNum; i++) {
+    let json = `{"name":"${nftName} #${i}","description":"${description}","image":"${hiddenImgUrl}","attributes":[{"trait_type": "Unknown","value": "Unknown"}]}`;
+    let fs = require("fs");
+    fs.writeFile(`json/${i}.json`, json, "utf8", (e) => e);
+  }
+  console.log("complete!");
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
